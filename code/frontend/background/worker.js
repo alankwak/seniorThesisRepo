d33d0984e719b7f03ffb9d3a77a1fcff8bcc908b
Alan <alan@alankwak.dev>
2025-11-14 21:36:51 -0600

    store groupId in local storage, persists across worker restarts

--- code/frontend/background/worker.js
+++ code/frontend/background/worker.js
@@ -1,13 +1,17 @@
 let cachedUserId = null;
 let activeSession = false;
-let sharedGroupId = null;
+let cachedGroupId = null;
+
+chrome.storage.local.get("sharedGroupId", data => {
+  cachedGroupId = data.sharedGroupId || null;
+});
 
 chrome.runtime.onStartup.addListener(initUserId);
 chrome.runtime.onInstalled.addListener(initUserId);
 
 function initUserId() {
   chrome.storage.local.get("userId", data => {
-    if (data.userId) {
+    if(data.userId) {
       cachedUserId = data.userId;
     } else {
       cachedUserId = crypto.randomUUID();
@@ -27,10 +31,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     sendResponse(activeSession);
   }
   if(message.action === "updateGroupId") {
-    sharedGroupId = message.message || null;
+    cachedGroupId = message.message || null;
+    chrome.storage.local.set({ sharedGroupId: cachedGroupId });
   }
   if(message.action === "getGroupId") {
-    sendResponse(sharedGroupId);
+    sendResponse(cachedGroupId);
   }
   if(message.action === "startSession") {
     activeSession = true;
@@ -41,7 +46,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if(tab.groupId === sharedGroupId) {
+  if(tab.groupId === cachedGroupId) {
     console.log("New tab in group");
     if(changeInfo.url) {
       console.log(`New URL: ${changeInfo.url}`);
@@ -51,10 +56,11 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 
 chrome.tabGroups.onRemoved.addListener( async (tabGroup) => {
-  if(tabGroup.id === sharedGroupId) {
-    const stillExists = await chrome.tabs.query({groupId: sharedGroupId});
+  if(tabGroup.id === cachedGroupId) {
+    const stillExists = await chrome.tabs.query({groupId: cachedGroupId});
     if(stillExists.length == 0) {
-      sharedGroupId = null;
+      cachedGroupId = null;
+      chrome.storage.local.remove("sharedGroupId");
     }
   }
 });
