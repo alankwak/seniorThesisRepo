bb0c03cd857a187cc25c1abc9fedb64a12d0be34
Alan <alan@alankwak.dev>
2026-04-09 00:52:03 -0500

    use local server for dev and hosted server for full build

--- code/frontend/background/worker.js
+++ code/frontend/background/worker.js
@@ -63,7 +63,7 @@ async function connectSocket() {
     }
 
     if(!socket) {
-      socket = io("http://localhost:3000", {
+      socket = io(import.meta.env.VITE_WS_SERVER_URL, {
         reconnection: false,
         transports: ["websocket"]
       });
